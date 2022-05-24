2e2bf03ff3e50d63267c7d7290ffa3e3be284a32
German Distel <germandistel@gmail.com>
2022-05-24 10:22:01 -0300

    Completes tests for /post

--- backend/src/post/post.controller.test.ts
+++ backend/src/post/post.controller.test.ts
@@ -6,6 +6,7 @@ import CreatePostDto from './post.dto';
 import Post from './post.entity';
 
 describe('PostController ', () => {
+
     it('POST /post', () => {
         const postData: CreatePostDto = {
             title: 'My new post',
@@ -17,7 +18,7 @@ describe('PostController ', () => {
             save: () => Promise.resolve(),
         });
         const postsController = new PostController();
-        const appInstance = new App([postsController]);    
+        const appInstance = new App([postsController]);
         return request(appInstance.app)
             .post(`${postsController.path}`)
             .send(postData)
@@ -27,6 +28,28 @@ describe('PostController ', () => {
                 expect(res.body.imageUrl).toBe(postData.imageUrl);
             });
     });
+
+    it('PATCH /post/:id', () => {
+        const updatedPost: Post = {
+            id: 1,
+            title: 'My new post',
+            content: 'This is the content',
+            imageUrl: 'https://www.google.com',
+            comments: []
+        };
+        AppDataSource.getRepository = jest.fn().mockReturnValue({
+            findOneBy: () => (updatedPost),
+            update: () => Promise.resolve(),
+        });
+        const postsController = new PostController();
+        const appInstance = new App([postsController]);
+        return request(appInstance.app)
+            .patch(`${postsController.path}/1`)
+            .expect((res) => {
+                expect(res.body).toEqual(updatedPost);
+            });
+    });
+
     it('GET /post', () => {
         const posts: Post[] = [
             { id: 1, title: 'My post', content: 'blog post', imageUrl: 'https://www.google.com', comments: [] }
@@ -35,11 +58,45 @@ describe('PostController ', () => {
             find: () => (posts),
         });
         const postsController = new PostController();
-        const appInstance = new App([postsController]);    
+        const appInstance = new App([postsController]);
         return request(appInstance.app)
             .get(`${postsController.path}`)
             .expect((res) => {
                 expect(res.body).toEqual(posts);
             });
+    });
+
+    it('GET /post/:id', () => {
+        const post: Post = {
+            id: 1, title: 'My post', content: 'blog post',
+            imageUrl: 'https://www.google.com', comments: []
+        };
+        AppDataSource.getRepository = jest.fn().mockReturnValue({
+            createQueryBuilder: (entity: string) => ({
+                where: () => ({
+                    leftJoinAndSelect: () => ({
+                        getOne: () => (post)
+                    })
+                })
+            }),
+        });
+        const postsController = new PostController();
+        const appInstance = new App([postsController]);
+        return request(appInstance.app)
+            .get(`${postsController.path}/${post.id}`)
+            .expect((res) => {
+                expect(res.body).toEqual({ post });
+            });
+    });
+
+    it('DELETE /post/:id', () => {
+        AppDataSource.getRepository = jest.fn().mockReturnValue({
+            delete: () => ({ affected: 1 })
+        });
+        const postsController = new PostController();
+        const appInstance = new App([postsController]);
+        return request(appInstance.app)
+            .delete(`${postsController.path}/1`)
+            .expect(200);
     });
 });
